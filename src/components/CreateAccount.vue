8590def0598716764f70010835fe956df7da7cf6
ジョセフ·ダミアン·ガリード ☠ <damian.garrido.munoz@gmail.com>
2019-07-24 16:26:21 -0400

    create account and organization

--- src/components/CreateAccount.vue
+++ src/components/CreateAccount.vue
@@ -13,7 +13,8 @@
                 <b-form-input
                   id="input_name"
                   type="text"
-                  placeholder="Name"></b-form-input>
+                  placeholder="Name"
+                  v-model="user.first_name"></b-form-input>
               </b-form-group>
               <b-form-group
                 id="input_group_surname"
@@ -22,7 +23,8 @@
                 <b-form-input
                   id="input_surname"
                   type="text"
-                  placeholder="Surname"></b-form-input>
+                  placeholder="Surname"
+                  v-model="user.last_name"></b-form-input>
               </b-form-group>
               <b-form-group
                 id="input_group_email"
@@ -31,8 +33,16 @@
                 <b-form-input
                   id="input_email"
                   type="email"
+                  :state="ui.username_validation"
                   required
-                  placeholder="Enter a valid email"></b-form-input>
+                  aria-describedby="input-live-help input-live-feedback"
+                  placeholder="Enter a valid email"
+                  v-model="username"></b-form-input>
+                <b-form-text
+                  id="input-live-feedback"
+                  v-if="!ui.username_validation">This username exist!</b-form-text>
+                <b-form-text
+                  id="input-live-help">This will be used for loggin.</b-form-text>
               </b-form-group>
               <b-form-group
                 id="input_group_affiliation"
@@ -41,7 +51,8 @@
                 <b-form-input
                   id="input_affiliation"
                   type="text"
-                  placeholder="Your affiliation"></b-form-input>
+                  placeholder="Your affiliation"
+                  v-model="user.affiliation"></b-form-input>
               </b-form-group>
               <b-form-group
                 id="input_group_password"
@@ -51,7 +62,8 @@
                   id="input_password"
                   type="password"
                   required
-                  placeholder="Write an strong password with at least 8 alpha-numeric characters"></b-form-input>
+                  placeholder="Write an strong password with at least 8 alpha-numeric characters"
+                  v-model="password"></b-form-input>
               </b-form-group>
               <b-form-group
                 id="input_group_repeat_password"
@@ -61,7 +73,8 @@
                   id="input_repeat_password"
                   type="password"
                   required
-                  placeholder="Write the same password above"></b-form-input>
+                  placeholder="Write the same password above"
+                  v-model="password_2"></b-form-input>
               </b-form-group>
               <b-card-text class="text-center text-forgot-create">
                 <router-link :to="{name: 'Login'}">login</router-link> | <router-link :to="{name: 'ForgotPassword'}">forgot your password?</router-link>
@@ -70,10 +83,90 @@
                 slot="footer"
                 class="text-right">
                 <b-button
-                  variant="outline-primary">Create account</b-button>
+                  variant="outline-primary"
+                  :disabled="!(ui.username_validation && ui.password_validation)"
+                  @click="createAccount">Create account</b-button>
               </div>
             </b-card>
           </b-form>
+        </b-col>
+        <b-col class="mt-4" cols="12" md="10" lg="8" offset-md="1" offset-lg="2">
+          <b-card header="Join to an a organisation">
+            <p>
+              As an individual, you will be able to create iSoQF tables, but they will remain in your personal space (i.e. they will be only visible when logged in your account).
+            </p>
+            <p>
+              In order share iSoQF tables with others you need to join an existing organisation
+            </p>
+            <b-form-group
+              label="Organization"
+              label-for="input-select-organization">
+              <b-form-select
+                v-model="org_selected"
+                :options="all_organizations"
+                value-field="id"
+                text-field="name"></b-form-select>
+            </b-form-group>
+            <div
+              slot="footer"
+              class="text-right">
+              <b-button
+                variant="outline-primary"
+                @click="createAccount">Join</b-button>
+            </div>
+          </b-card>
+          <b-card header="Create an organisation">
+            <b-form-group
+              label="Name"
+              label-for="organization-name">
+              <b-form-input
+                id="organization-name"
+                type="text">
+              </b-form-input>
+            </b-form-group>
+            <b-form-group
+              label="Website"
+              label-for="organization-website">
+              <b-form-input
+                id="organization-website"
+                type="url"
+                placeholder="https://myorganization.org">
+              </b-form-input>
+            </b-form-group>
+            <b-form-group>
+              <b-form-checkbox
+                v-model="nonprofit"
+                value="false"
+                unchecked-value="true">confirm this is a non-profit organisation</b-form-checkbox>
+            </b-form-group>
+            <b-form-group label="Plan">
+              <b-form-radio-group
+                id="option-plans"
+                v-model="selected_plan"
+                :options="plans"></b-form-radio-group>
+            </b-form-group>
+            <b-row>
+              <b-col
+                cols="12" sm="4"
+                v-for="(plan, index) in plans"
+                :key="index">
+                <b-card
+                  :header="plan.text">
+                  <ul class="list-unstyled">
+                    <li>users: {{plan.specs.users}}</li>
+                    <li>tables: {{plan.specs.tables}}</li>
+                    <li>price: {{plan.specs.price}}</li>
+                  </ul>
+                </b-card>
+              </b-col>
+            </b-row>
+            <div
+              slot="footer"
+              class="text-right">
+              <b-button
+                variant="outline-primary">Create an Join</b-button>
+            </div>
+          </b-card>
         </b-col>
       </b-row>
     </b-container>
@@ -81,8 +174,117 @@
 </template>
 
 <script>
-export default {
+import axios from 'axios'
+import _debounce from 'lodash.debounce'
 
+export default {
+  data () {
+    return {
+      ui: {
+        username_validation: null,
+        password_validation: false
+      },
+      organizations: [
+        {id: 'examples', name: 'Examples'},
+        {id: 'episte', name: 'Test organization'}
+      ],
+      user: {
+        first_name: '',
+        last_name: '',
+        affiliation: ''
+      },
+      username: '',
+      password: '',
+      password_2: '',
+      org_selected: '',
+      all_organizations: [],
+      nonprofit: true,
+      selected_plan: false,
+      plans: [
+        {text: 'Small', value: 'small', specs: {users: 10, tables: 20, price: 'free'}},
+        {text: 'Medium', value: 'medium', specs: {users: 50, tables: 200, price: '1000 USD a year'}},
+        {text: 'Large', value: 'large', specs: {users: 'Contact us', tables: 'Contact us', price: 'Contact us'}}
+      ]
+    }
+  },
+  watch: {
+    username: function (email) {
+      this.checkEmail()
+    },
+    password: function () {
+      this.comparePassword()
+    },
+    password_2: function () {
+      this.comparePassword()
+    }
+  },
+  mounted () {
+    this.getOrganizations()
+  },
+  created: function () {
+    this.checkEmail = _debounce(this.checkEmailExist, 500)
+  },
+  methods: {
+    createAccount: function () {
+      this.user.password = this.password
+      this.user.username = this.user
+      let params = {
+        user: this.user,
+        organizations: this.organizations
+      }
+      axios.post('/create_user', params)
+        .then((response) => {
+          console.log(response)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    },
+    checkEmailExist: function () {
+      let params = {
+        params: {
+          email: this.username
+        }
+      }
+      axios.get('/users/check_email', params)
+        .then((response) => {
+          if (response.data.error === false) {
+            this.ui.username_validation = true
+          } else {
+            this.ui.username_validation = false
+          }
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    },
+    comparePassword: function () {
+      let password = this.password.trim()
+      let password2 = this.password_2.trim()
+      this.ui.password_validation = false
+
+      if (password !== '' && password2 !== '') {
+        if (password === password2) {
+          this.ui.password_validation = true
+        }
+      }
+    },
+    getOrganizations: function () {
+      let params = {
+        params: {
+          personal_organization: false
+        }
+      }
+      axios.get('/api/organizations', params)
+        .then((response) => {
+          console.log(response.data)
+          this.all_organizations = response.data
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    }
+  }
 }
 </script>
 
