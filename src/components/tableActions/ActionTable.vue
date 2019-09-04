4bbbd12dfcb8a3dc58a613c20ab29c744d82c656
damian garrido <dgarrido@localhost.localdomain>
2019-09-04 16:08:23 -0400

    upgrade actions component

--- src/components/tableActions/ActionTable.vue
+++ src/components/tableActions/ActionTable.vue
@@ -7,10 +7,11 @@
         <b-col
           class="my-1"
           cols="12"
-          sm="3">
+          sm="3"
+          v-if="displayDeleteTable">
           <b-button
             block
-            @click="dropAction"
+            @click="modalDropTable"
             variant="outline-danger">
               <font-awesome-icon icon="table" />
               Delete table
@@ -19,10 +20,11 @@
         <b-col
           class="my-1"
           cols="12"
-          sm="3">
+          sm="3"
+          v-if="displayEditTable">
           <b-button
             block
-            @click="editAction"
+            @click="modalEditTable"
             variant="outline-primary">
               <font-awesome-icon icon="table" />
               Edit table
@@ -31,10 +33,11 @@
         <b-col
           class="my-1"
           cols="12"
-          sm="3">
+          sm="3"
+          v-if="displayImport">
           <b-button
             block
-            v-b-modal.modal-stage-import-tsv
+            @click="modalImport"
             variant="outline-success">
               <font-awesome-icon icon="file-upload" />
               {{$t('Import data')}}
@@ -43,13 +46,27 @@
         <b-col
           class="my-1"
           cols="12"
-          sm="3">
+          sm="3"
+          v-if="displayCreateContent">
           <b-button
             block
-            @click="addAction"
+            @click="modalCreateContent"
             variant="outline-success">
               <font-awesome-icon icon="plus" />
               {{$t('Add data')}}
+          </b-button>
+        </b-col>
+        <b-col
+          class="my-1"
+          cols="12"
+          sm="3"
+          v-if="displayCreateTable">
+          <b-button
+            block
+            @click="modalCreateTable"
+            variant="outline-primary">
+              <font-awesome-icon icon="table" />
+              Create table
           </b-button>
         </b-col>
       </b-row>
@@ -63,15 +80,9 @@
       @ok="importTSV"
       @cancel="cleanTSV"
       title="Import data">
-      {{importUrl}}
+      <b-alert show variant="warning"><b>Important!</b> Import data will destroy the loaded previous data.</b-alert>
       <b-form-group
-        label="Paste your TSV file">
-        <b-form-textarea
-          placeholder="Paste your TSV file"
-          rows="3"></b-form-textarea>
-      </b-form-group>
-      <b-form-group
-        label="OR load a TSV file"
+        label="Upload a TSV file"
         label-for="input-tsv-file">
         <b-form-file
           id="input-tsv-file"
@@ -86,6 +97,89 @@
       </div>
     </b-modal>
     <!-- end import modal -->
+    <!-- modal drop table -->
+    <b-modal
+      id="modal-drop-table"
+      title="Drop table"
+      ref="modal-drop-table"
+      @ok="dropTable">
+      <p>This action will remove all the data. Are you sure?</p>
+    </b-modal>
+    <!-- end modal drop table-->
+    <!-- create study tables -->
+    <b-modal
+      title="Add fields"
+      ref="modal-create-fields"
+      @ok="saveCreateFields">
+      <b-form-group
+        id="nro-of-fields"
+        label="Nro of fields"
+        label-for="number-of-fields">
+        <b-form-input
+          id="number-of-fields"
+          type="number"
+          v-model="modalCreateFields.nro_of_fields"></b-form-input>
+        </b-form-group>
+      <b-form-group
+        v-for="item in parseInt(modalCreateFields.nro_of_fields)"
+        :key="item"
+        :label="`Column name #${item}`"
+        :label-for="`field-${item}`">
+        <b-form-input
+        :id="`field-${item}`"
+        type="text"
+        v-model="modalCreateFields.fields[item - 1]"></b-form-input>
+        </b-form-group>
+    </b-modal>
+    <!-- end of create study tables -->
+    <!-- modal edit fields -->
+    <b-modal
+      title="Edit fields"
+      ref="modal-edit-fields"
+      @ok="saveUpdateFields"
+      @cancel="cancelModalFields">
+      <b-form-group
+        v-for="(item, index) in modalEditFields"
+        :key="index"
+        :label="`Column #${index}`"
+        :label-for="`field-${index}`">
+        <b-input-group>
+          <b-form-input
+            :id="`field-${index}`"
+            type="text"
+            v-model="item.label"></b-form-input>
+          <b-input-group-append
+            v-if="modalEditFields.length > 1">
+            <b-button
+              @click="removeField(item, index)">
+              <font-awesome-icon
+                icon="trash"></font-awesome-icon>
+            </b-button>
+          </b-input-group-append>
+        </b-input-group>
+      </b-form-group>
+      <b-button
+        variant="outline-success"
+        @click="addNewColumn">Add column</b-button>
+    </b-modal>
+    <!-- end of modal edit fields -->
+    <!-- create study data -->
+    <b-modal
+      title="Add data"
+      ref="modal-add-data"
+      @ok="saveNewData">
+      <b-form-group
+        v-for="(item, index) in modalAddData.fields"
+        :key="index"
+        :label="`${item.label}`"
+        :label-for="`data-${index}`">
+        <b-form-textarea
+          :id="`data-${index}`"
+          row="3"
+          v-model="newItem[item.key]"></b-form-textarea>
+      </b-form-group>
+    </b-modal>
+    <!-- end of create study data -->
   </div>
 </template>
 
@@ -96,17 +190,32 @@ export default {
   name: 'ActionTable',
   props: {
     importUrl: '',
-    dropAction: '',
-    editAction: '',
-    addAction: ''
+    displayDeleteTable: false,
+    displayEditTable: false,
+    displayImport: false,
+    displayCreateTable: false,
+    displayCreateContent: false,
+    theContent: {}
   },
-  data () {
+  data: function () {
     return {
       pre_tsv: '',
       tsv: {
         fields: [],
         items: []
-      }
+      },
+      modalCreateFields: {
+        nro_of_fields: 1,
+        fields: []
+      },
+      modalEditFields: [],
+      modalContent: {},
+      lastFieldColumnId: 0,
+      modalAddData: {
+        fields: [],
+        items: []
+      },
+      newItem: {}
     }
   },
   methods: {
@@ -140,18 +249,173 @@ export default {
       this.$refs['modal-stage-import-tsv'].hide()
     },
     importTSV: function () {
-      console.log('aaaaa')
       if (this.tsv.items.length) {
-        axios.patch(`${this.importUrl}`, this.tsv)
-          .then((response) => {
-            console.log(response)
-          })
-          .catch((error) => {
-            console.log(error)
-          })
+        let params = {}
+        let urlParams = this.importUrl.split('?')[1].split('&')
+        for (let p of urlParams) {
+          let key = p.split('=')[0]
+          let value = p.split('=')[1]
+          params[key] = value
+        }
+        params.fields = this.tsv.fields
+        params.items = this.tsv.items
+
+        if (this.importUrl.split('?')[0].split('/').length === 4) {
+          axios.put(`${this.importUrl}`, params)
+            .then((response) => {
+              this.$emit('response-ok-api')
+            })
+            .catch((error) => {
+              console.log(error)
+            })
+        } else {
+          axios.post(`${this.importUrl}`, params)
+            .then((response) => {
+              this.$emit('response-ok-api')
+            })
+            .catch((error) => {
+              console.log(error)
+            })
+        }
       }
+    },
+    modalDropTable: function () {
+      this.$refs['modal-drop-table'].show()
+    },
+    modalEditTable: function () {
+      this.modalContent = JSON.parse(JSON.stringify(this.theContent))
+      let fields = JSON.parse(JSON.stringify(this.theContent.fields))
+      fields.splice(fields.length - 1, 1)
+      this.modalEditFields = fields
+
+      this.$refs['modal-edit-fields'].show()
+    },
+    modalImport: function () {
+      this.$refs['modal-stage-import-tsv'].show()
+    },
+    modalCreateContent: function () {
+      this.modalContent = JSON.parse(JSON.stringify(this.theContent))
+      let fields = JSON.parse(JSON.stringify(this.theContent.fields))
+      fields.splice(fields.length - 1, 1)
+
+      this.modalAddData.fields = fields
+      this.$refs['modal-add-data'].show()
+    },
+    saveNewData: function () {
+      let params = {}
+      let urlParams = this.importUrl.split('?')[1].split('&')
+      for (let p of urlParams) {
+        let key = p.split('=')[0]
+        let value = p.split('=')[1]
+        params[key] = value
+      }
+      this.modalContent.items.push(this.newItem)
+
+      params.items = this.modalContent.items
+
+      axios.patch(`${this.importUrl}`, params)
+        .then((response) => {
+          this.newItem = {}
+          this.modalContent = {}
+          this.$emit('response-ok-api')
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    },
+    createTableOpenModal: function () {},
+    dropTable: function () {
+      axios.delete(`${this.importUrl}`)
+        .then((response) => {
+          this.$emit('response-ok-api')
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    },
+    modalCreateTable: function () {
+      this.$refs['modal-create-fields'].show()
+    },
+    saveCreateFields: function () {
+      let fields = JSON.parse(JSON.stringify(this.modalCreateFields.fields))
+      let params = JSON.parse(JSON.stringify(this.modalCreateFields))
+      params.fields = []
+
+      for (let cnt in fields) {
+        let objField = {}
+        objField.key = 'column_' + cnt
+        objField.label = fields[cnt]
+        params.fields.push(objField)
+      }
+
+      let urlParams = this.importUrl.split('?')[1].split('&')
+      for (let p of urlParams) {
+        let key = p.split('=')[0]
+        let value = p.split('=')[1]
+        params[key] = value
+      }
+
+      axios.post(`${this.importUrl}`, params)
+        .then((response) => {
+          this.modalCreateFields = {
+            nro_of_fields: 1,
+            fields: []
+          }
+          this.$emit('response-ok-api')
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    },
+    saveUpdateFields: function () {
+      let params = {}
+
+      if (this.modalContent.hasOwnProperty('fields')) {
+        params.fields = JSON.parse(JSON.stringify(this.modalEditFields))
+      }
+
+      for (let cnt in params.fields) {
+        if (params.fields[cnt].label === '') {
+          params.fields.splice(cnt, 1)
+        }
+      }
+
+      let urlParams = this.importUrl.split('?')[1].split('&')
+      for (let p of urlParams) {
+        let key = p.split('=')[0]
+        let value = p.split('=')[1]
+        params[key] = value
+      }
+
+      axios.patch(`${this.importUrl}`, params)
+        .then((response) => {
+          this.modalContent = {}
+          this.modalEditFields = []
+          // this.getStageThree()
+          this.$emit('response-ok-api')
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    },
+    addNewColumn: function () {
+      let theFields = JSON.parse(JSON.stringify(this.modalEditFields))
+      let lastFieldColumnId = parseInt(theFields.splice(theFields.length - 1, 1)[0]['key'].split('_')[1]) + 1
+      let newField = {
+        key: 'column_' + lastFieldColumnId,
+        label: ''
+      }
+      this.modalEditFields.push(newField)
+    },
+    cancelModalFields: function () {
+      this.modalEditFields = {}
+      this.$refs['modal-edit-fields'].hide()
+    },
+    removeField: function (item, index) {
+      this.modalEditFields.splice(index, 1)
     }
   }
+
 }
 </script>
 
