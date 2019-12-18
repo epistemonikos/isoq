8dc9a2138f2086c9e5742869851838a08f172327
damian garrido <dgarrido@localhost.localdomain>
2019-12-18 11:56:37 -0300

    change organization title by workspace

--- src/components/Organizations.vue
+++ src/components/Organizations.vue
@@ -1,17 +1,33 @@
 <template>
   <div class="mt-4">
     <b-container>
-      <h2 class="mb-5">{{ $t("Organizations") }}</h2>
+      <h2 class="mb-5">Workspaces</h2>
       <b-row>
         <b-col
+          sm="6"
           cols="12">
+          <h3>My Workspace</h3>
+          <b-table
+            id="my-workspace"
+            striped
+            :fields="fields"
+            :items="myOrganization">
+            <template v-slot:cell(name)="data">
+              <b-link :to="{ name: 'viewOrganization', params: { id: data.item.id } }">{{ data.item.name }}</b-link>
+            </template>
+          </b-table>
+        </b-col>
+        <b-col
+          sm="6"
+          cols="12">
+          <h3>Other Workspaces</h3>
           <b-table
             id="organizations"
             striped
             :fields="fields"
-            :items="organizations">
+            :items="otherOrganizations">
             <template v-slot:cell(name)="data">
-              <b-link :to="{name: 'viewOrganization', params: {id: data.item.id}}">{{ (data.item.name === 'My frameworks') ? 'My iSoQf' : data.item.name }}</b-link>
+              <b-link :to="{ name: 'viewOrganization', params: { id: data.item.id } }">{{ data.item.name }}</b-link>
             </template>
           </b-table>
         </b-col>
@@ -25,7 +41,8 @@ export default {
   data () {
     return {
       default_logo_org: '//placehold.it/800x600',
-      organizations: [],
+      myOrganization: [],
+      otherOrganizations: [],
       fields: [
         { key: 'name', label: 'Name' }
       ]
@@ -37,7 +54,12 @@ export default {
   methods: {
     getOrganizations: function () {
       for (let org of this.$store.state.user.orgs) {
-        this.organizations.push(org)
+        if (org.name === 'My frameworks' || org.name === 'My iSoQf') {
+          org.name = 'My iSoQf'
+          this.myOrganization.push(org)
+        } else {
+          this.otherOrganizations.push(org)
+        }
       }
     }
   }
