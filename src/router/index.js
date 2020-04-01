b36a9e5b3af5eaa44360ab7050eedf49cbb97185
damian garrido <dgarrido@localhost.localdomain>
2020-04-01 15:38:42 -0300

    route simplify

--- src/router/index.js
+++ src/router/index.js
@@ -10,114 +10,99 @@ import EditList from '@/components/list/editList'
 import ViewProject from '@/components/project/viewProject'
 import Browse from '@/components/Browse'
 
-import { Trans } from '@/plugins/Translation'
+// import { Trans } from '@/plugins/Translation'
 
 var routes = [
   {
-    path: '/:lang',
-    component: {
-      template: '<router-view></router-view>'
-    },
-    beforeEnter: Trans.routeMiddleware,
-    children: [
-      {
-        path: '',
-        name: 'MainPage',
-        component: MainPage,
-        meta: {
-          title: 'iSoF Qualy'
-        }
-      },
-      {
-        path: '/about',
-        name: 'About',
-        component: About,
-        meta: {
-          title: 'iSoF Qualy - About',
-          requiresAuth: true
-        }
-      },
-      {
-        path: '/login',
-        name: 'Login',
-        component: Login,
-        meta: {
-          title: 'iSoF Qualy - Login'
-        }
-      },
-      {
-        path: '/forgot_password',
-        name: 'ForgotPassword',
-        component: ForgotPassword,
-        meta: {
-          title: 'iSoF Qualy - Forgot password'
-        }
-      },
-      {
-        path: '/create_an_account',
-        name: 'CreateAccount',
-        component: CreateAccount,
-        meta: {
-          title: 'iSoF Qualy - Create an account'
-        }
-      },
-      {
-        path: '/organizations',
-        name: 'Organizations',
-        component: Organizations,
-        meta: {
-          title: 'iSoF Qualy - Organizations',
-          requiresAuth: true
-        }
-      },
-      {
-        path: '/organization/:id',
-        name: 'viewOrganization',
-        component: ViewOrganization,
-        meta: {
-          title: 'iSoF Qualy - Organization'
-        }
-      },
-      {
-        path: '/organization/:org_id/project/:id',
-        name: 'viewProject',
-        component: ViewProject,
-        meta: {
-          requiresAuth: true
-        }
-      },
-      {
-        path: '/list/:id',
-        name: 'viewList',
-        component: ViewList,
-        meta: {
-          title: 'iSoF Qualy - List',
-          requiresAuth: true
-        }
-      },
-      {
-        path: '/list/:id/edit',
-        name: 'editList',
-        component: EditList,
-        meta: {
-          title: 'iSoF Qualy - Edit List',
-          requiresAuth: true
-        }
-      },
-      {
-        path: '/browse',
-        name: 'Browse',
-        component: Browse,
-        meta: {
-          title: 'iSoF Qualy - Browse'
-        }
-      }
-    ]
+    path: '',
+    name: 'MainPage',
+    component: MainPage,
+    meta: {
+      title: 'interactive Summaries of Quality findings'
+    }
   },
   {
-    path: '*',
-    redirect (to) {
-      return Trans.getUserSupportedLang()
+    path: '/about',
+    name: 'About',
+    component: About,
+    meta: {
+      title: 'interactive Summaries of Quality findings - About',
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login,
+    meta: {
+      title: 'interactive Summaries of Quality findings - Login'
+    }
+  },
+  {
+    path: '/forgot_password',
+    name: 'ForgotPassword',
+    component: ForgotPassword,
+    meta: {
+      title: 'interactive Summaries of Quality findings - Forgot password'
+    }
+  },
+  {
+    path: '/create_an_account',
+    name: 'CreateAccount',
+    component: CreateAccount,
+    meta: {
+      title: 'interactive Summaries of Quality findings - Create an account'
+    }
+  },
+  {
+    path: '/organizations',
+    name: 'Organizations',
+    component: Organizations,
+    meta: {
+      title: 'interactive Summaries of Quality findings - Organizations',
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/organization/:id',
+    name: 'viewOrganization',
+    component: ViewOrganization,
+    meta: {
+      title: 'interactive Summaries of Quality findings - Organization'
+    }
+  },
+  {
+    path: '/organization/:org_id/project/:id',
+    name: 'viewProject',
+    component: ViewProject,
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/list/:id',
+    name: 'viewList',
+    component: ViewList,
+    meta: {
+      title: 'interactive Summaries of Quality findings - List',
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/list/:id/edit',
+    name: 'editList',
+    component: EditList,
+    meta: {
+      title: 'interactive Summaries of Quality findings - Edit List',
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/browse',
+    name: 'Browse',
+    component: Browse,
+    meta: {
+      title: 'interactive Summaries of Quality findings - Browse'
     }
   }
 ]
