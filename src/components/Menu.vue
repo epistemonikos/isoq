457ff90983677078a3bfea1b4ca62d2ac6203214
damian garrido <dgarrido@localhost.localdomain>
2020-04-13 13:05:11 -0400

    fix display of title

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -3,7 +3,7 @@
     <b-navbar id="main-navbar" toggleable="lg">
       <b-navbar-brand :to="{name: 'MainPage'}">
         <img :src="iconUrl" alt="GRADE CERQual">
-        interactive Summary of Quality Findings
+        <div>interactive Summary of Quality Findings</div>
       </b-navbar-brand>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
