3bad48eb03ec39e95d9c16899a814c1a253acada
Damian Garrido <damian.garrido.munoz@gmail.com>
2020-08-14 10:53:09 -0400

    change content

--- src/components/About.vue
+++ src/components/About.vue
@@ -13,19 +13,30 @@
         <li>Assist review authors with managing and archiving data for GRADE-CERQual assessments.</li>
         <li>Make GRADE-CERQual assessments more accessible to end users, including decision makers and those who support them.</li>
       </ul>
+
       <h3>What is GRADE-CERQual?</h3>
-      <p>GRADE-CERQual is an approach for transparently assessing and describing how much confidence decision makers and other users can place in individual review findings from syntheses (or systematic reviews) of qualitative evidence. An overall GRADE-CERQual assessment of confidence is based on an assessment of four components: methodological limitations, coherence, adequacy and relevance.</p>
+      <p>GRADE-CERQual (www.cerqual.org) is an approach for transparently assessing and describing how much confidence decision makers and other users can place in individual review findings from syntheses (or systematic reviews) of qualitative evidence. An overall GRADE-CERQual assessment of confidence is based on an assessment of four components: methodological limitations, coherence, adequacy and relevance.</p>
+
       <h3>When in the review process can I use the iSoQ tool?</h3>
-      <p>You can begin using this tool once you have finished the study screening process and have a list of included studies that meet the inclusion criteria for your synthesis (systematic review). In the early stages of a systematic review, the iSoQ tool can be used to help organise some of the data that are needed to apply GRADE-CERQual, for example, tables of the characteristics of included studies and of the methodological limitations of the included studies (critical appraisal tables). </p>
+      <p>You can begin using this tool once you have finished the study screening process and have a list of included studies that meet the inclusion criteria for your synthesis (systematic review). In the early stages of a systematic review, the iSoQ tool can be used to help organise some of the data that are needed to apply GRADE-CERQual, for example, tables of the characteristics of included studies and of the methodological limitations of the included studies (critical appraisal tables).</p>
       <p>The primary function of the tool is to assist review authors with managing the process of applying the GRADE-CERQual approach to their review findings. GRADE-CERQual is applied near the end of a review process, at the point when reviewers have completed analysis and synthesis and have draft summaries of each review finding.</p>
+
       <h3>Does the iSoQ tool replace other kinds of software used in reviews?</h3>
-      <p>No, this tool is specific to applying the GRADE-CERQual approach. It is not a replacement for other commonly used software in systematic reviews like reference management software (e.g. Endnote), software for screening and selecting studies (e.g. Covidence and Distiller SR), or software and programs for extracting and analyzing data (e.g. DistillerSR, NVivo, Microsoft Excel).</p>
-      <h3>Can you invite collaborators to an iSoQ?</h3>
-      <p>Yes. You can invite collaborators (for example other review authors) to a shared project and assign them either “view only” or “view and edit” rights. These collaborators need to set-up an iSoQ account in order to access the shared project. You can also generate a temporary link to send to collaborators who only need to view the project for a limited amount of time (for example a guideline panel or a peer reviewer).</p>
-      <h3>Can I export from iSoQ into other programs?</h3>
-      <p>Currently you cannot directly export the iSoQ into other platforms but you can export and download a Microsoft Word or PDF copy of the Summary of Qualitative Findings Table and Evidence Profile Table. These can be copy and pasted or imported into other programs such as RevMan, GRADEpro GDT, or MAGICapp. Characteristics of Studies, Methodological Assessments and Extracted Data tables created in iSoQ can be downloaded into a Microsoft Excel compatible format.</p>
-      <h3>Can iSoQs be made public for anyone to see?</h3>
-      <p>iSoQ includes a “publish” function, meaning you can publish some or all of your completed Interactive Summary of Qualitative Findings Table to the iSoQ database. It is up to you to decide if and when to publish to the iSoQ database and to ensure you are not infringing any copyright issues (for example you may have to abide by an embargo period or wait for a manuscript to be published). The database will become a repository for iSoQ tables.</p>
+      <p>No, this tool is specific to applying the GRADE-CERQual approach. It is not a replacement for other commonly used software in systematic reviews like reference management software (e.g. Endnote), software for screening and selecting studies (e.g. Covidence and Distiller SR), or software and programs for extracting and analysing data (e.g. DistillerSR, NVivo, Microsoft Excel).</p>
+
+      <h3>Can you invite collaborators to an iSoQ project?</h3>
+      <p>Yes. You can invite collaborators (for example other review authors) to a shared project and assign them either "view only" or "view and edit" rights. These collaborators need to set-up an iSoQ account in order to access the shared project. You can also generate a temporary link to send to collaborators who only need to view the project for a limited amount of time (for example a guideline panel or a peer reviewer).</p>
+
+      <h3>Can I export from the iSoQ tool into other programs?</h3>
+      <p>Currently you cannot directly export the content within the iSoQ tool into other platforms, but you can export and download a Microsoft Word or PDF copy of the Summary of Qualitative Findings Table and Evidence Profile Tables. These can be copy and pasted or imported into other programs such as RevMan, GRADEpro GDT, or MAGICapp. Characteristics of Studies, Methodological Assessments and Extracted Data tables created in the iSoQ tool can be downloaded into a Microsoft Excel compatible format.</p>
+
+      <h3>Can iSoQ tables be made public for anyone to see?</h3>
+      <p>iSoQ includes a "publish" function, meaning you can publish some or all of your completed Interactive Summary of Qualitative Findings Table to the iSoQ database. It is up to you to decide if and when to publish to the iSoQ database and to ensure you are not infringing any copyright issues (for example you may have to abide by an embargo period or wait for a manuscript to be published). The database will become a repository for iSoQ tables.</p>
+
+      <h3>How was the iSoQ tool funded?</h3>
+      <p>The iSoQ tool was developed with funding from the Norwegian Institute of Public Health’s Centre for Informed Health Choices.</p>
+
+      <h3>How is the iSoQ tool and its contents licenced?</h3>
     </b-container>
   </div>
 </template>
