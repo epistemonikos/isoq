7b71232e5f2e4f48a5348ea0b652ec92933486e0
damian-garrido <damian@epistemonikos.org>
2024-07-01 08:54:55 -0400

    chore: Add banner component to App.vue for displaying event information

---
+++ src/components/banner/event.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <b-alert show :variant="ui.variant" class="text-center">{{ ui.text }} <a :href="ui.url" target="_blank">{{ ui.url_text }}</a></b-alert>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Event',
+  data () {
+    return {
+      ui: {
+        error: null,
+        text: 'Attend a live demo on September 26, 2024.',
+        url: 'https://training.cochrane.org/zoom/meeting/XnMukn%252BaRhayM%252FJlVxkicA%253D%253D/signup',
+        url_text: 'Sign up here',
+        variant: 'primary'
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .alert {
+    border-radius: 0;
+  }
+</style>
