0ed17602cda078de11bf668e5b2a44486ee1e1da
MJWainwright <72507850+MJWainwright@users.noreply.github.com>
2024-08-19 12:57:22 +0100

    Update WhatsNew.vue

--- src/components/WhatsNew.vue
+++ src/components/WhatsNew.vue
@@ -9,7 +9,14 @@
       <!-- desde aqui -->
       <h3>What's new in iSoQ?</h3>
       <p>Here you will find the latest updates and improvements to iSoQ Version 1.0.0.</p>
-      <h3>March 2024</h3>
+   <h3>May 2024</h3>
+      <!-- acá escribes tu comentario -->
+      <p>Facilitating access to information on iSoQ development </p>
+      <ul>
+        <li>
+          Users are now prompted to sign-up to the GRADE-CERQual newsletter when they create an account or change their password. GRADE-CERQual newsletters are sent three times a year and include the latest news about the iSoQ platform, upcoming training or webinars, and other resources for applying the GRADE-CERQual approach
+        <li> 
+    <h3>April 2024</h3>
       <!-- acá escribes tu comentario -->
       <p>We have redesigned the My Data page for easier navigation</p>
       <ul>
@@ -23,21 +30,30 @@
           Scroll over the Authors names to see the title of the paper in Steps 3&4
         </li>
       </ul>
-       <h3>March 2024</h3>
+    <h3>February 2024</h3>
       <!-- acá escribes tu comentario -->
-      <p>We have redesigned the My Data page for easier navigation</p>
+      <p>No more jumping! </p>
       <ul>
         <li>
-          A table of contents on the left makes visualising and moving between each step easy
+          Previously iSoQ defaulted to jumping to the top of the page when the user navigated between pages. Now iSoQ holds your spot on the page thus reducing scroll time. 
+        </li>
+         <li>
+          Previously iSoQ defaulted to jumping to the top of the page when the user navigated between pages. Now iSoQ holds your spot on the page thus reducing scroll time. 
+        </li>
+         You will notice this enhancement especially when navigating back and forth between the iSoQ table and he GRADE-CERQual assessment worksheets for each finding. 
+        </li>
+      </ul>   
+    <h3>October 2023</h3>
+      <!-- acá escribes tu comentario -->
+      <p>Leaving a project simplified </p>
+      <ul>
+        <li>
+           Users can now leave a project that was shared with them by clicking the "leave" button in My Workspace
         </li>
         <li>
-          Edit and delete buttons for each row in the tables (step 3&4) remain visible even when scrolling
+          This will remove the project entirely from My Workspace 
         </li>
-        <li>
-          Scroll over the Authors names to see the title of the paper in Steps 3&4
-        </li>
-      </ul>
-      
+      </ul>  
       <!-- hasta aqui -->
     </b-container>
   </div>
