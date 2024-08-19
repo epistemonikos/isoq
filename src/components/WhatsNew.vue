a349d53ddcaa16186414d6cd5af38bf04e3619cc
MJWainwright <72507850+MJWainwright@users.noreply.github.com>
2024-08-19 11:47:07 +0100

    Update WhatsNew.vue

--- src/components/WhatsNew.vue
+++ src/components/WhatsNew.vue
@@ -8,10 +8,8 @@
     <b-container class="py-5">
       <!-- desde aqui -->
       <h3>What's new in iSoQ?</h3>
-      <p>Here you will find the latest updates and improvements to iSoQ.</p>
-      <h3>Version 1.0.0</h3>
-      <p>Initial release of iSoQ.</p>
-      <p>new content</p>
+      <p>Here you will find the latest updates and improvements to iSoQ Version 1.0.0.</p>
+      <h3>March 2024</h3>
       <!-- acá escribes tu comentario -->
       <p>We have redesigned the My Data page for easier navigation</p>
       <ul>
@@ -25,6 +23,21 @@
           Scroll over the Authors names to see the title of the paper in Steps 3&4
         </li>
       </ul>
+       <h3>March 2024</h3>
+      <!-- acá escribes tu comentario -->
+      <p>We have redesigned the My Data page for easier navigation</p>
+      <ul>
+        <li>
+          A table of contents on the left makes visualising and moving between each step easy
+        </li>
+        <li>
+          Edit and delete buttons for each row in the tables (step 3&4) remain visible even when scrolling
+        </li>
+        <li>
+          Scroll over the Authors names to see the title of the paper in Steps 3&4
+        </li>
+      </ul>
+      
       <!-- hasta aqui -->
     </b-container>
   </div>
