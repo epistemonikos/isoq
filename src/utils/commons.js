24caef5dd44503272bd209acf035c17e803108b2
damian-garrido <damian@epistemonikos.org>
2024-10-01 12:55:58 -0300

    add a new component for display table of soqs Refactor Commons class to improve error handling

--- src/utils/commons.js
+++ src/utils/commons.js
@@ -58,4 +58,31 @@ export default class Commons {
       return ''
     }
   }
+
+  static printErrors (error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      // console.log(error.response.data)
+      // console.log(error.response.status)
+      // console.log(error.response.headers)
+      return {
+        data: error.response.data,
+        status: error.response.status,
+        headers: error.response.headers
+      }
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+      // console.log(error.request)
+      return { request: error.request }
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      // console.log('Error', error.message)
+      return { message: error.message }
+    }
+    // console.log(error.config)
+    // return { config: error.config }
+  }
 }
