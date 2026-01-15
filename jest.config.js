11858bf1266e3aa45243aab7d07711490c42f232
damian-garrido <damian@epistemonikos.org>
2026-01-15 12:31:55 -0300

    chore: configure Jest testing infrastructure
    - Install Jest dependencies (jest@27.5.1, @vue/test-utils@1.3.0, vue-jest@3.0.7, babel-jest@27.5.1)
    - Install babel-core bridge for Babel 7 compatibility
    - Create jest.config.js with Vue 2 configuration
    - Create tests/unit/setup.js with Vue mocks (i18n, router, store)
    - Add test scripts to package.json (test, test:watch, test:coverage)
    - Add sanity test to verify Jest is working correctly
    - Tests passing: 2/2

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'vue'],
+  transform: {
+    '^.+\\.vue$': 'vue-jest',
+    '^.+\\.js$': 'babel-jest'
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  testMatch: [
+    '**/tests/unit/**/*.spec.js'
+  ],
+  collectCoverageFrom: [
+    'src/components/**/*.vue',
+    'src/mixins/**/*.js',
+    'src/utils/**/*.js',
+    '!src/components/**/index.js',
+    '!**/node_modules/**'
+  ],
+  testEnvironment: 'jsdom',
+  // setupFilesAfterEnv: ['<rootDir>/tests/unit/setup.js'],
+  coverageDirectory: '<rootDir>/coverage',
+  coverageReporters: ['html', 'text', 'lcov'],
+  verbose: true
+}
---
+++ tests/unit/setup.js
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+
+// Suppress Vue warnings during tests
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+// Mock i18n
+Vue.prototype.$t = (key) => key
+Vue.prototype.$tc = (key) => key
+
+// Mock router
+Vue.prototype.$router = {
+  push: jest.fn(),
+  replace: jest.fn(),
+  go: jest.fn(),
+  back: jest.fn()
+}
+
+Vue.prototype.$route = {
+  path: '/',
+  params: {},
+  query: {}
+}
+
+// Mock store
+Vue.prototype.$store = {
+  state: {},
+  getters: {},
+  commit: jest.fn(),
+  dispatch: jest.fn()
+}
+
+// Mock window.print
+global.print = jest.fn()
+
