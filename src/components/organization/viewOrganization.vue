6fb1409d0cff9a2554ed836dcba342a24ef71fa6
damian-garrido <damian.garrido.munoz@gmail.com>
2026-03-25 15:38:15 -0300

    filter added

--- src/components/organization/viewOrganization.vue
+++ src/components/organization/viewOrganization.vue
@@ -10,47 +10,39 @@
         <h3>{{ $t("menu.projects") }}</h3>
         <b-row align-h="end" v-if="$store.state.user.personal_organization === this.$route.params.id">
           <b-col cols="12" class="text-right">
-            <b-button
-              v-b-tooltip.hover
-              :title="(isOnline) ? $t('project.create_new_isoq') : $t('offline.action_disabled')"
-              variant="success"
-              :disabled="!isOnline"
-              @click="openModalNewFindingTable">{{ $t("project.add_new") }}</b-button>
+            <b-button v-b-tooltip.hover
+              :title="(isOnline) ? $t('project.create_new_isoq') : $t('offline.action_disabled')" variant="success"
+              :disabled="!isOnline" @click="openModalNewFindingTable">{{ $t("project.add_new") }}</b-button>
+          </b-col>
+        </b-row>
+        <b-row class="mt-3" v-if="projects.length > 10">
+          <b-col cols="12" md="6" class="ml-auto">
+            <b-form-group>
+              <b-input-group>
+                <b-form-input v-model="searchQuery" type="search" id="filterInput"
+                  :placeholder="$t('common.search')"></b-form-input>
+                <b-input-group-append>
+                  <b-button :disabled="!searchQuery" @click="searchQuery = ''">{{ $t('common.clear') }}</b-button>
+                </b-input-group-append>
+              </b-input-group>
+            </b-form-group>
           </b-col>
         </b-row>
         <b-row class="mt-3">
           <b-col cols="12">
-            <b-table
-              id="organizations"
-              responsive
-              striped
-              hover
-              head-variant="light"
-              :busy="ui.projectTable.isBusy"
-              :fields="ui.projectTable.fields"
-              :items="projects"
-              sort-by="created_at"
-              :sort-desc="true">
+            <b-table id="organizations" responsive striped hover head-variant="light" :busy="ui.projectTable.isBusy"
+              :fields="ui.projectTable.fields" :items="filteredProjects" sort-by="created_at" :sort-desc="true">
               <template v-slot:cell(private)="data">
-                <b-badge
-                  variant="light"
-                  class="publish-status"
-                  v-b-tooltip.hover
+                <b-badge variant="light" class="publish-status" v-b-tooltip.hover
                   :title="(global_status.find(obj => obj.value === data.item.public_type) || {}).text">
-                  {{ (global_status.find(obj => obj.value === data.item.public_type) || {}).transValue }}
+                  {{(global_status.find(obj => obj.value === data.item.public_type) || {}).transValue}}
                 </b-badge>
               </template>
               <template v-slot:cell(name)="data">
-                <b-link
-                  :id="`p-${data.item.id}`"
-                  class="link-project"
-                  :to="{name: 'viewProject', params: {org_id: data.item.organization, id: data.item.id}}">
+                <b-link :id="`p-${data.item.id}`" class="link-project"
+                  :to="{ name: 'viewProject', params: { org_id: data.item.organization, id: data.item.id } }">
                   {{ data.item.name }}
-                  <b-badge
-                    v-if="data.item.use_camelot"
-                    variant="info"
-                    class="ml-2"
-                    v-b-tooltip.hover
+                  <b-badge v-if="data.item.use_camelot" variant="info" class="ml-2" v-b-tooltip.hover
                     title="This project uses CAMELOT">
                     C
                   </b-badge>
@@ -58,56 +50,53 @@
               </template>
               <template v-slot:cell(actions)="data">
                 <div class="d-block d-lg-none">
-                  <b-dropdown id="dropdown-1" :text="$t('project.project_options')" class="m-md-2" variant="outline-secondary">
-                    <b-dropdown-item v-if="data.item.is_owner || data.item.allow_to_write" @click="openCloneModal(data.item)" link-class="text-decoration-none"><font-awesome-icon icon="copy"></font-awesome-icon> {{ $t('common.duplicate') }}</b-dropdown-item>
-                    <b-dropdown-item v-if="data.item.is_owner && (data.item.sharedToken && data.item.sharedToken.length)" @click="modalShareOptions(data.item, 2)" link-class="text-decoration-none"><font-awesome-icon icon="link"></font-awesome-icon> {{ $t('common.shared') }}</b-dropdown-item>
-                    <b-dropdown-item v-if="data.item.is_owner" @click="modalShareOptions(data.item)" link-class="text-decoration-none"><font-awesome-icon icon="users"></font-awesome-icon> {{ $t('common.share') }}</b-dropdown-item>
-                    <b-dropdown-item v-if="data.item.allow_to_write" @click="openModalEditProject(data.item)" link-class="text-decoration-none"><font-awesome-icon icon="edit"></font-awesome-icon>{{ $t('common.edit') }}</b-dropdown-item>
-                    <b-dropdown-item v-if="data.item.is_owner" @click="modalRemoveProject(data.item)" link-class="text-decoration-none"><font-awesome-icon icon="trash"></font-awesome-icon> {{ $t('common.remove') }}</b-dropdown-item>
-                    <b-dropdown-item v-if="!data.item.is_owner && (data.item.allow_to_write || data.item.allow_to_read)" @click="openModalLeaveProject(data.item)" link-class="text-decoration-none"><font-awesome-icon icon="sign-out-alt"></font-awesome-icon> {{ $t('common.leave') }}</b-dropdown-item>
+                  <b-dropdown id="dropdown-1" :text="$t('project.project_options')" class="m-md-2"
+                    variant="outline-secondary">
+                    <b-dropdown-item v-if="data.item.is_owner || data.item.allow_to_write"
+                      @click="openCloneModal(data.item)" link-class="text-decoration-none"><font-awesome-icon
+                        icon="copy"></font-awesome-icon> {{ $t('common.duplicate') }}</b-dropdown-item>
+                    <b-dropdown-item
+                      v-if="data.item.is_owner && (data.item.sharedToken && data.item.sharedToken.length)"
+                      @click="modalShareOptions(data.item, 2)" link-class="text-decoration-none"><font-awesome-icon
+                        icon="link"></font-awesome-icon> {{ $t('common.shared') }}</b-dropdown-item>
+                    <b-dropdown-item v-if="data.item.is_owner" @click="modalShareOptions(data.item)"
+                      link-class="text-decoration-none"><font-awesome-icon icon="users"></font-awesome-icon> {{
+                        $t('common.share') }}</b-dropdown-item>
+                    <b-dropdown-item v-if="data.item.allow_to_write" @click="openModalEditProject(data.item)"
+                      link-class="text-decoration-none"><font-awesome-icon icon="edit"></font-awesome-icon>{{
+                        $t('common.edit') }}</b-dropdown-item>
+                    <b-dropdown-item v-if="data.item.is_owner" @click="modalRemoveProject(data.item)"
+                      link-class="text-decoration-none"><font-awesome-icon icon="trash"></font-awesome-icon> {{
+                        $t('common.remove') }}</b-dropdown-item>
+                    <b-dropdown-item v-if="!data.item.is_owner && (data.item.allow_to_write || data.item.allow_to_read)"
+                      @click="openModalLeaveProject(data.item)" link-class="text-decoration-none"><font-awesome-icon
+                        icon="sign-out-alt"></font-awesome-icon> {{ $t('common.leave') }}</b-dropdown-item>
                   </b-dropdown>
                 </div>
                 <div class="d-none d-lg-block">
-                  <b-button
-                    v-if="data.item.is_owner || data.item.allow_to_write"
-                    :title="$t('common.duplicate')"
-                    variant="outline-secondary"
-                    @click="openCloneModal(data.item)">
+                  <b-button v-if="data.item.is_owner || data.item.allow_to_write" :title="$t('common.duplicate')"
+                    variant="outline-secondary" @click="openCloneModal(data.item)">
                     <font-awesome-icon icon="copy"></font-awesome-icon>
                   </b-button>
-                  <b-button
-                    v-if="data.item.is_owner && (data.item.sharedToken && data.item.sharedToken.length)"
-                    :title="$t('project.temp_link_warning')"
-                    variant="outline-secondary"
+                  <b-button v-if="data.item.is_owner && (data.item.sharedToken && data.item.sharedToken.length)"
+                    :title="$t('project.temp_link_warning')" variant="outline-secondary"
                     @click="modalShareOptions(data.item, 2)">
                     <font-awesome-icon icon="link"></font-awesome-icon>
                   </b-button>
-                  <b-button
-                    v-if="data.item.is_owner"
-                    :title="$t('common.share')"
-                    variant="outline-secondary"
+                  <b-button v-if="data.item.is_owner" :title="$t('common.share')" variant="outline-secondary"
                     @click="modalShareOptions(data.item)">
                     <font-awesome-icon icon="users"></font-awesome-icon>
                   </b-button>
-                  <b-button
-                    v-if="data.item.allow_to_write"
-                    :title="$t('common.edit')"
-                    variant="outline-success"
+                  <b-button v-if="data.item.allow_to_write" :title="$t('common.edit')" variant="outline-success"
                     @click="openModalEditProject(data.item)">
                     <font-awesome-icon icon="edit"></font-awesome-icon>
                   </b-button>
-                  <b-button
-                    v-if="data.item.is_owner"
-                    :title="$t('common.remove')"
-                    variant="outline-danger"
+                  <b-button v-if="data.item.is_owner" :title="$t('common.remove')" variant="outline-danger"
                     @click="modalRemoveProject(data.item)">
                     <font-awesome-icon icon="trash"></font-awesome-icon>
                   </b-button>
-                  <b-button
-                    v-if="!data.item.is_owner && (data.item.allow_to_write || data.item.allow_to_read)"
-                    :title="$t('common.leave')"
-                    variant="outline-success"
-                    @click="openModalLeaveProject(data.item)">
+                  <b-button v-if="!data.item.is_owner && (data.item.allow_to_write || data.item.allow_to_read)"
+                    :title="$t('common.leave')" variant="outline-success" @click="openModalLeaveProject(data.item)">
                     <font-awesome-icon icon="sign-out-alt"></font-awesome-icon>
                   </b-button>
                 </div>
@@ -123,54 +112,23 @@
         </b-row>
       </div>
 
-      <ProjectFormModal
-        ref="projectFormModal"
-        :project="buffer_project"
-        :canEditProject="canEditProject"
-        :lockedByUser="lockedByUser"
-        @cancel="cleanProject"
-        @project-saved="onProjectSaved"
-      />
+      <ProjectFormModal ref="projectFormModal" :project="buffer_project" :canEditProject="canEditProject"
+        :lockedByUser="lockedByUser" @cancel="cleanProject" @project-saved="onProjectSaved" />
 
-      <RemoveProjectModal
-        ref="removeProjectModal"
-        :project="buffer_project"
-        :usersAllowed="users_allowed"
-        @processing="setProcessing"
-        @cancel="cleanProject"
-        @project-removed="onProjectRemoved"
-      />
+      <RemoveProjectModal ref="removeProjectModal" :project="buffer_project" :usersAllowed="users_allowed"
+        @processing="setProcessing" @cancel="cleanProject" @project-removed="onProjectRemoved" />
 
-      <ShareProjectModal
-        ref="shareProjectModal"
-        :project="buffer_project"
-        :usersAllowed="users_allowed"
-        :initialTab="ui.tabIndex"
-        @hidden="cleanProject"
-        @processing="setProcessing"
-        @project-shared="onProjectShared"
-        @user-unshared="onUserUnshared"
-        @invited-unshared="onInvitedUnshared"
-        @permission-changed="onPermissionChanged"
-      />
+      <ShareProjectModal ref="shareProjectModal" :project="buffer_project" :usersAllowed="users_allowed"
+        :initialTab="ui.tabIndex" @hidden="cleanProject" @processing="setProcessing" @project-shared="onProjectShared"
+        @user-unshared="onUserUnshared" @invited-unshared="onInvitedUnshared"
+        @permission-changed="onPermissionChanged" />
 
-      <CloneProjectModal
-        ref="cloneProjectModal"
-        :project="buffer_project"
-        :uiCopy="ui.copy"
-        @update-copy-state="updateCopyState"
-        @clone-started="onCloneStarted"
-        @project-cloned="onProjectCloned"
-        @cancel="cleanProject"
-      />
+      <CloneProjectModal ref="cloneProjectModal" :project="buffer_project" :uiCopy="ui.copy"
+        @update-copy-state="updateCopyState" @clone-started="onCloneStarted" @project-cloned="onProjectCloned"
+        @cancel="cleanProject" />
 
-      <LeaveProjectModal
-        ref="leaveProjectModal"
-        :project="buffer_project"
-        @processing="setProcessing"
-        @cancel="cleanProject"
-        @project-left="onProjectLeft"
-      />
+      <LeaveProjectModal ref="leaveProjectModal" :project="buffer_project" @processing="setProcessing"
+        @cancel="cleanProject" @project-left="onProjectLeft" />
 
     </b-container>
   </div>
@@ -195,7 +153,7 @@ export default {
     CloneProjectModal,
     LeaveProjectModal
   },
-  data () {
+  data() {
     return {
       ui: {
         projectTable: {
@@ -258,15 +216,31 @@ export default {
       buffer_project: {},
       users_allowed: [],
       projects: [],
+      searchQuery: '',
       hashId: null,
       canEditProject: false,
       lockedByUser: null
     }
   },
-  created () {
+  computed: {
+    filteredProjects() {
+      if (!this.searchQuery) return this.projects
+      const query = this.searchQuery.toLowerCase()
+      return this.projects.filter(project => {
+        return (
+          (project.name && project.name.toLowerCase().includes(query)) ||
+          (project.description && project.description.toLowerCase().includes(query)) ||
+          (project.review_question && project.review_question.toLowerCase().includes(query)) ||
+          (project.author && project.author.toLowerCase().includes(query)) ||
+          (project.list_authors && project.list_authors.toLowerCase().includes(query))
+        )
+      })
+    }
+  },
+  created() {
     this.buffer_project = JSON.parse(JSON.stringify(this.tmp_buffer_project))
   },
-  mounted () {
+  mounted() {
     this.getProjects()
   },
   methods: {
@@ -394,7 +368,7 @@ export default {
       this.buffer_project = _project
 
       let basePermission = (this.$store.state.user.personal_organization === this.$route.params.id) ||
-                           (project.allow_to_write)
+        (project.allow_to_write)
 
       if (basePermission) {
         const res = await LockService.acquire(project.id)
@@ -530,28 +504,27 @@ export default {
 </script>
 
 <style scoped>
-  div >>>
-    .publish-status {
-      text-transform: uppercase;
-    }
-  div >>>
-    .link-project {
-      color: #000;
-    }
-  div >>>
-    table#organizations thead th:nth-child(2) {
-      width: 60%;
-    }
-  div >>>
-    table#organizations thead th:last-child {
-      width: 30%;
-    }
-  div >>>
-    table#organizations tbody td:last-child {
-      text-align: right;
-    }
-  div >>>
-    table#organizations tbody td a {
-      text-decoration: underline;
-    }
+div>>>.publish-status {
+  text-transform: uppercase;
+}
+
+div>>>.link-project {
+  color: #000;
+}
+
+div>>>table#organizations thead th:nth-child(2) {
+  width: 60%;
+}
+
+div>>>table#organizations thead th:last-child {
+  width: 30%;
+}
+
+div>>>table#organizations tbody td:last-child {
+  text-align: right;
+}
+
+div>>>table#organizations tbody td a {
+  text-decoration: underline;
+}
 </style>
